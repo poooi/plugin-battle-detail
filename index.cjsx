f8105afde5f2da4d833a025e8d077f91a898e9d9
Dazzy Ding <h@dazzyd.org>
2016-06-08 09:27:57 +0800

    Compile index.cjsx to index.js

--- index.cjsx
+++
@@ -1,8 +0,0 @@
-module.exports =
-  windowOptions:
-    x: config.get 'poi.window.x', 0
-    y: config.get 'poi.window.y', 0
-    width: 850
-    height: 650
-  windowURL: "file://#{__dirname}/battle-detail.html"
-  useEnv: true
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  windowOptions: {
+    x: config.get('poi.window.x', 0),
+    y: config.get('poi.window.y', 0),
+    width: 850,
+    height: 650,
+  },
+  windowURL: "file://" + __dirname + "/battle-detail.html",
+  useEnv: true,
+}
